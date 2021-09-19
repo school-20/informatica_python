daabbafbf87249bb969d47a55df277304fe5522d
Vladimir Ivanov <mrhoustonvr@yandex.ru>
2021-09-19 21:38:37 +0400

    added new way for done task 1

--- task1.py
+++ task1.py
@@ -3,9 +3,21 @@
 while True:
     number = input("Input two-digit number or -1 for exit: ")
     if number == "-1":
-        print("Goodbye! Thank you for using my programm.")
         break
     if len(number) == 2 and number[0].isdigit() and number[1].isdigit():
-        print(int(number[0]) - int(number[1]))
+        result = abs(int(number[0]) - int(number[1]))
+        print(result)
     else:
         print("Input number is not correct, please try again")
+
+# Решение второе, используя арифметические операции. 
+
+# while True:
+#     x = input("введите двузначное число или -1 для выхода: ")
+#     if x == "-1":
+#         break
+#     if len(x) == 2 and x[0].isdigit() and x[1].isdigit():
+#         print(abs(int(x) // 10 - int(x) % 10))
+#     else:
+#         print("Input number is not correct, please try again")
+print("Goodbye! Thank you for using my programm.")
